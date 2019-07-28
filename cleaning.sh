8ac20f194174c69ad959d926d4b667725b124498
jerryUNSW <z5201685@unsw.edu.au>
2019-07-28 15:34:00 +1000

    to macbook

--- cleaning.sh
+++ cleaning.sh
@@ -19,28 +19,29 @@ cat quote_completed | egrep -o "\{.*\}" | tr "'" "\"" > quote_completed_payload
 paste quote_completed_case quote_completed_payload -d'~' > quote_completed_combined 
 
 # clean the quote_completed data
+cat quote_completed|egrep -o "'address'\: .*\}"|cut -d':' -f2|tr -d "}" | tr -d "'"> quote_address
 cat quote_completed | sed -e s/\{.*\}//g > quote_completed_basic 
 cat quote_completed_basic | cut -d'-' -f1 | sed -e "s/[\" ]//g"> case_id 
 cat quote_completed_basic | cut -d'-' -f2 | sed -e "s/ //g" > plat_form 
 cat quote_completed_basic | cut -d'-' -f3 | egrep -o ": [a-z0-9]{6}" | cut -d' ' -f2 > customer
 cat quote_completed_basic | cut -d',' -f2 > timeStamp
-paste case_id plat_form customer timeStamp -d',' > quote_completed_basic
-rm case_id plat_form customer timeStamp quote_completed_case quote_completed_payload
+paste case_id plat_form customer quote_address timeStamp -d',' > quote_completed_basic
+rm case_id plat_form customer timeStamp quote_completed_case quote_completed_payload quote_address
 
-# clean the claim_started data 
-cat claim_started | cut -d'-' -f1 | sed -e "s/[\" ]//g"> case_id 
-cat claim_started | cut -d'-' -f2 | sed -e "s/ //g" > plat_form 
-cat claim_started | cut -d'-' -f3 | egrep -o ": [a-z0-9]{6}" | cut -d' ' -f2 > customer
-cat claim_started | cut -d',' -f2 > timeStamp
-paste case_id plat_form customer timeStamp -d',' > claim_started_basic 
-rm case_id plat_form customer timeStamp
+# # clean the claim_started data 
+# cat claim_started | cut -d'-' -f1 | sed -e "s/[\" ]//g"> case_id 
+# cat claim_started | cut -d'-' -f2 | sed -e "s/ //g" > plat_form 
+# cat claim_started | cut -d'-' -f3 | egrep -o ": [a-z0-9]{6}" | cut -d' ' -f2 > customer
+# cat claim_started | cut -d',' -f2 > timeStamp
+# paste case_id plat_form customer timeStamp -d',' > claim_started_basic 
+# rm case_id plat_form customer timeStamp
 
-# clean the claim_accepted data 
-cat claim_accepted | cut -d'-' -f1 | sed -e "s/[\" ]//g"> case_id 
-cat claim_accepted | cut -d'-' -f2 | sed -e "s/ //g" > plat_form 
-cat claim_accepted | cut -d'-' -f3 | egrep -o ": [a-z0-9]{6}" | cut -d' ' -f2 > customer
-cat claim_accepted | cut -d',' -f2 > timeStamp
-cat claim_accepted| egrep -o -i "paid.*,"| egrep -o "[0-9]+\.?[0-9]*" > paid_amount
+# # clean the claim_accepted data 
+# cat claim_accepted | cut -d'-' -f1 | sed -e "s/[\" ]//g"> case_id 
+# cat claim_accepted | cut -d'-' -f2 | sed -e "s/ //g" > plat_form 
+# cat claim_accepted | cut -d'-' -f3 | egrep -o ": [a-z0-9]{6}" | cut -d' ' -f2 > customer
+# cat claim_accepted | cut -d',' -f2 > timeStamp
+# cat claim_accepted| egrep -o -i "paid.*,"| egrep -o "[0-9]+\.?[0-9]*" > paid_amount
 
-paste case_id paid_amount plat_form customer timeStamp -d',' > claim_accepted_basic 
-rm case_id plat_form customer timeStamp paid_amount
+# paste case_id paid_amount plat_form customer timeStamp -d',' > claim_accepted_basic 
+# rm case_id plat_form customer timeStamp paid_amount
